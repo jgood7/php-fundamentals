87f0944f9ada22dc4bcbdf7fcbe1701553469a9a
joshg <joshgood211@gmail.com>
2025-04-09 00:03:31 -0400

    feat: add form control for  text box, radio buttons, checkbox, submit button. Add red text style to styles.css. Add minor information to README.md.
    Radio buttons iteratively generate using a key-value pair and display two per line.
    
    Text box requires content and will return an alert if nothing is provided.
    
    All form data is handled over a POST and is preserved when submitted.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # php-fundamentals
 Used for housing some basic html pages with embedded php as a learning environment
+
+CLI Flags:
+    -f: Defines filepath from current directory for launching
+    -S: Defines server/port to launch on
+
+    Ex. `php -S localhost:8000 -f public`
+
+Regarding updates, changes, and checks for content:
+    Server-Side: php
+    Client-Side: javascript
--- public/index.php
+++ public/index.php
@@ -23,6 +23,43 @@ require_once "../includes/header.php";
         <p>This is the home page of my website.</p>
         <p>Nothing is here yet.</p>
         <p>Feel free to explore, but you won't get far!</p>
+        <?php
+            $fname = $_POST['fname'] ?? '';
+            $checked = isset($_POST['checkbox1']);
+            $radio=$_POST['radio_cluster'] ?? '';
+            $radio_options=[
+            'first' => 'Option 1',
+            'second' => 'Option 2',
+            'third' => 'Option 3',
+            'fourth' => 'Option 4',
+            'fifth' => 'Option 5',
+            'sixth' => 'Option 6',
+
+            ];
+            $i=0;
+        ?>
+        <form name='index_form' method='post' onsubmit="return validate()">
+            <label for='fname'>Text Input:</label><br>
+            <input type='text' id='fname' name='fname' value="<?php echo htmlspecialchars($fname);?>"><span class='err'>*</span><br>
+            <?php foreach ($radio_options as $value => $label): ?>
+                <input type='radio' id='<?php echo $value;?>' name='radio_cluster' value='<?php echo $value;?>'
+                    <?php if ($radio===$value) echo 'checked'; ?>>
+                   <?php echo $label;?>
+                        <?php if ($i%2===1) echo '<br>';
+                        $i++;?>
+                   <?php endforeach;?>
+            <input type='checkbox' id='check' name='checkbox1' value='checked'<?php if ($checked) echo 'checked'; ?>> This is a checkbox<br>
+            <input type='submit' name='submit_btn' value='Submit Above Content'>
+        </form>
+    <script>
+        function validate() {
+            var x = document.forms['index_form']['fname'].value;
+            if (x==""){
+            alert("Text input must be provided.");
+            return false;
+            }
+        }
+    </script>
     </main>
     </body>
     </html>
