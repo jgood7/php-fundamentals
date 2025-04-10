917ac43d51584491572ba2bd38e599fcd425c74a
joshg <joshgood211@gmail.com>
2025-04-09 21:41:00 -0400

    feat: add env usage and required dependency info, mongo login field in bootstrap.php

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,5 +1,13 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
+require_once __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . "/../.env")){
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+    $dotenv -> load();
+    }
+else{
+    die("Error: .env file not found.");
+    }
+$mongo_login = $_ENV['MONGO_LOGIN'];
+$client = new MongoDB\Client($mongo_login);
 ?>
